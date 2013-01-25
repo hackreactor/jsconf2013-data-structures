b0ef23a8c11fc4afb5639d33eadfb7b013de01bc
Marcus Phillips <github.com@marcusphillips.com>
2013-01-25 10:55:07 -0800

    new basic specs for existence of other methods

--- spec/queueSpec.js
+++ spec/queueSpec.js
@@ -7,6 +7,8 @@ describe("queue", function() {
 
   it("should have a method named 'add'", function() {
     expect(queue.add).toEqual(jasmine.any(Function));
+    expect(queue.remove).toEqual(jasmine.any(Function));
+    expect(queue.size).toEqual(jasmine.any(Function));
   });
 
   // add more tests here to test the functionality of queue
--- spec/stackSpec.js
+++ spec/stackSpec.js
@@ -7,6 +7,8 @@ describe("stack", function() {
 
   it("should have a method named 'add'", function() {
     expect(stack.add).toEqual(jasmine.any(Function));
+    expect(stack.remove).toEqual(jasmine.any(Function));
+    expect(stack.size).toEqual(jasmine.any(Function));
   });
 
   // add more tests here to test the functionality of stack
